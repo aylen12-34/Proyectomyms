fa1377221ef159c3f3b842a60a86c95c609ffd3f
hagointrosgratis12-design <hagointrosgratis12@gmail.com>
2026-04-08 09:16:00 -0400

    Add description field to task registration

--- registrotareas.php
+++ registrotareas.php
@@ -1,8 +1,11 @@
 <?php
     require "bd.php"
     $nombre=$_POST['nombre'];
-    $direccion=$_POST['direc'];
+    $descripcion=$_POST['descript'];
     $estado=$_POST['estado'];
 
-    $sql
+    $sql ="INSERT INTO tareas (nombre,descript,estado) VALUES ('$nombre',' $descripcion','$estado')";
+    if($conexion->query($sql)=== TRUE){
+        echo"Se registro la informacion";
+    }
 ?>
